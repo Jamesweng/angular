381db72be143072ccc544645007bb7ba4539096a
Jamesweng <cnshoit@gmail.com>
2022-06-28 23:21:54 +0800

    fix(http): encode + signs in query params as %2B (#11058)

--- packages/common/http/src/params.ts
+++ packages/common/http/src/params.ts
@@ -96,7 +96,6 @@ function standardEncoding(v: string): string {
       .replace(/%24/gi, '$')
       .replace(/%2C/gi, ',')
       .replace(/%3B/gi, ';')
-      .replace(/%2B/gi, '+')
       .replace(/%3D/gi, '=')
       .replace(/%3F/gi, '?')
       .replace(/%2F/gi, '/');
